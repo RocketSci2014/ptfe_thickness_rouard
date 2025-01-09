9bfa478c1b9d14b5f7eec908409555579a951987
Bo Wang <bevan.wang.2011@gmail.com>
2025-01-09 18:32:14 +0800

    Added read_data_script.py

---
+++ read_data_script.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+This script reads the raw data and extract refractive index and absorption rate. Study the rest of the project and reuse
+the logic that applies to the questions below.
+
+@author: Bo Wang
+@file: read_data_script.py
+@time: 1/9/2025 3:24 PM
+"""
+# Read data from data/离轴7.5°聚四氟乙烯薄片.xls and then extract timings and amplitudes of the THz signal.
+
+# Regarding the reference signal and the signal reflected from film, figure out how to calculate the phase delay.
+
+# Study the FFT (fast Fourier transform) and figure out how to convert the reference signal and reflected signal to
+# frequency domain.
+
+# Figure out how to simulate the reflected signal based on the reference signal using the aforementioned phase delay.
+
+# Figure out how to convert the simulated signal to time domain and build the optimization function using MSE.

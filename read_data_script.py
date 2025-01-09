59ab1577ca2d02852465c8aedf031083c992cb59
Bo Wang <bevan.wang.2011@gmail.com>
2025-01-09 20:36:58 +0800

    minor change

--- read_data_script.py
+++ read_data_script.py
@@ -7,13 +7,12 @@ the logic that applies to the questions below.
 @file: read_data_script.py
 @time: 1/9/2025 3:24 PM
 """
-# Read data from data/离轴7.5°聚四氟乙烯薄片.xls and then extract timings and amplitudes of the THz signal.
+# 1. Read data from data/离轴7.5°聚四氟乙烯薄片.xls and then extract timings and amplitudes of the THz signal.Regarding the
+# reference signal and the signal reflected from film, figure out how to calculate the phase delay.
 
-# Regarding the reference signal and the signal reflected from film, figure out how to calculate the phase delay.
-
-# Study the FFT (fast Fourier transform) and figure out how to convert the reference signal and reflected signal to
+# 2. Study the FFT (fast Fourier transform) and figure out how to convert the reference signal and reflected signal to
 # frequency domain.
 
-# Figure out how to simulate the reflected signal based on the reference signal using the aforementioned phase delay.
+# 3. Figure out how to simulate the reflected signal based on the reference signal using the aforementioned phase delay.
 
-# Figure out how to convert the simulated signal to time domain and build the optimization function using MSE.
+# 4. Figure out how to convert the simulated signal to time domain and build the optimization function using MSE.
